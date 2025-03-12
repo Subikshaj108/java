e384271bf41582833831655cf0fa0dae7880421e
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-12 15:37:10 +0530

    introducing myself

---
+++ concate.java
@@ -0,0 +1,11 @@
+public class concate
+{
+    public static void main(String[] args)
+    {
+        String firstname="Subi";
+        String lastname="ksha";
+        String fullname=firstname+" "+lastname;
+        System.out.println("fullname:"+fullname);
+       
+    }
+}
