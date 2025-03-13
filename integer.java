a3b1e0c0f523805d46933d54f2e56b0d84e54f13
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 10:32:34 +0530

    narrow casting

---
+++ integer.java
@@ -0,0 +1,10 @@
+public class integer
+{
+    public static void main(String[] args)
+    {
+        double mydouble=6.5;
+        int myint=(int)mydouble;
+        System.out.println(myint);
+        
+    }
+}
