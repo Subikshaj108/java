309e3bc086eede850b02d4053e0b3295a55bd603
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 12:02:23 +0530

    logical operator

---
+++ logic.java
@@ -0,0 +1,34 @@
+public class logic
+{
+    public static void main(String[] args)
+    {
+        int num1=100;
+        int num2=67;
+        if(num1>num2 && num1!=num2)
+        {
+            System.out.println("it is true");
+
+        }
+        else
+        {
+
+        System.out.println("its is false");
+        }
+        int num3=50;
+        int num4=78;
+        if(num3>num4 || num3==num4)
+        {
+            System.out.println("it is true");
+
+        }
+        else
+        {
+
+        System.out.println("its is false");
+        }
+    }
+}       
+
+        
+    
+     
