ad9640a1e9322ec97c5fe4f47fde2c66137a52da
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 14:14:44 +0530

    string length

---
+++ strings.java
@@ -0,0 +1,11 @@
+public class strings
+{
+    public static void main(String[] args)
+    {
+        String a="where is my apple";
+        int len=a.length();
+        System.out.println("the length of apple:"+ len);
+        
+        
+    }
+}
