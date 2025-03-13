40edad212cd5f16a43a2f1aa85985ce4ce9b5f10
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 13:34:26 +0530

    swapping operation

---
+++ swap.java
@@ -0,0 +1,19 @@
+public class swap
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=6;
+        System.out.println("before swapping");
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after wapping");
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+
+
+    }
+}
