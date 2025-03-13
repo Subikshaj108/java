2d0891dbcd69bf94771f5c307811181d62d05783
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 11:08:50 +0530

    arithmetic operation

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+public class arithmetic
+{
+    public static void main(String[] args)
+    {
+        int x=10;
+        int y=20;
+        int z=30;
+       int a=x+y+z;
+     int b=x*y*z;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
