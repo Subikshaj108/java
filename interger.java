f5057894cc2780d528df339205806d4af514b853
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 10:23:39 +0530

    widening casting

---
+++ interger.java
@@ -0,0 +1,10 @@
+public class interger
+{
+    public static void main(String[] args)
+    {
+        int myint=9;
+        double mydouble=myint;
+        System.out.println(mydouble);
+        
+    }
+}
