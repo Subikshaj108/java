7a6431ce2bcb66e3467475b9495b70a9c478879f
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 15:13:26 +0530

    zero number

---
+++ condi.java
@@ -0,0 +1,20 @@
+public class condi
+{
+    public static void main(String[] args)
+    {
+        int num=0;
+        if(num>0)
+        {
+            System.out.println("the number is positive");
+
+        }
+        else if(num<0)
+        {
+            System.out.println("the number is negative");
+         }
+        else
+        {
+            System.out.println("the number is zero");
+        }
+    }
+}
