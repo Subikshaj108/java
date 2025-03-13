dad469d13589c9727704b159aa369e1394e4dd34
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 14:41:31 +0530

    voters

---
+++ condition.java
@@ -0,0 +1,16 @@
+public class condition
+{
+    public static void main(String[] args)
+    {
+        int age=20;
+        if(age<=17)
+        {
+            System.out.println("not able to vote");
+        
+        }
+        else
+        {
+            System.out.println("able to vote");
+        }
+    }
+}
