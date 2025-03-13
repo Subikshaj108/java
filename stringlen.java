d847a0aa207c67e1f2d9244b7b4012595547e0e2
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 14:26:05 +0530

    string character

---
+++ stringlen.java
@@ -0,0 +1,12 @@
+public class stringlen
+{
+    public static void main(String[] args)
+    {
+        String a="where is \'my\' apple";
+        String b="where is \"my\" apple";
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+    }
+}
