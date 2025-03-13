48c79c4a097a9ca665b8815a627477e2bfff2cb8
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 09:48:36 +0530

    students details

---
+++ students.java
@@ -0,0 +1,14 @@
+public class students
+{
+    public static void main(String[] args)
+    {
+        String name="subiksha";
+        String subjectname="computerScience";
+        int mark=80;
+        char grade='A';
+        System.out.println(name);
+        System.out.println(subjectname);
+        System.out.println(mark);
+        System.out.println(grade);
+    }
+}
