b6ba9fc1e63f0cca9b547ca1e44e37f153857ea5
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-13 11:50:17 +0530

    comparison operator

---
+++ compare.java
@@ -0,0 +1,29 @@
+public class compare
+{
+    public static void main(String[] args)
+    {
+       String name1="APPLE";
+       String name2="MANGO";
+       if(name1==name2)
+       {
+        System.out.println("it is true");
+       }
+       else
+       {System.out.println("it is false");
+       }
+    
+    
+        int num1=100;
+        int num2=67;
+        if(num1>num2)
+        {
+            System.out.println("it is true");
+
+        }
+        else
+        {
+
+        System.out.println("its is false");
+        }
+    }
+}       
