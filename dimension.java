d42e1977295c17e0fc2e8b32907fb61f1264c00d
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-14 14:10:43 +0530

    two dimension

---
+++ dimension.java
@@ -0,0 +1,13 @@
+public class dimension
+{
+    public static void main(String[] args)
+    {
+        int[][] num={{10,20,30},{40,50,60}};
+        System.out.println(num[0][0]);
+         System.out.println(num[0][1]);
+          System.out.println(num[0][2]);
+           System.out.println(num[1][0]);
+            System.out.println(num[1][1]);
+             System.out.println(num[1][2]);
+    }
+} 
