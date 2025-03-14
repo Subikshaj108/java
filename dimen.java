0e72c2817e45d2b90ebfd29b551f155ac4787533
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-14 14:18:01 +0530

    three dimension

---
+++ dimen.java
@@ -0,0 +1,16 @@
+public class dimen
+{
+    public static void main(String[] args)
+    {
+        int [][][] num = {{{10, 20, 30}, {40, 50, 60}, {70, 80, 90}}};
+        System.out.println(num[0][0][0]); 
+        System.out.println(num[0][0][1]); 
+        System.out.println(num[0][0][2]); 
+        System.out.println(num[0][1][0]); 
+        System.out.println(num[0][1][1]); 
+        System.out.println(num[0][1][2]); 
+        System.out.println(num[0][2][0]); 
+        System.out.println(num[0][2][1]); 
+        System.out.println(num[0][2][2]); 
+    }
+}
