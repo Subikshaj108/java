3693b4d2a9d847fc0c4a83163240ef94ab5f4531
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-14 13:54:51 +0530

    length of fruit

---
+++ arrays.java
@@ -0,0 +1,12 @@
+public class arrays
+{
+    public static void main(String[] args)
+    {
+        String[] fruits={"apple","mango","orange","pineapple","strawberry"}; 
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+        System.out.println(fruits[3]);
+        System.out.println(fruits[4]);
+        System.out.println("length of the array:"+fruits.length);
+    }
+}
