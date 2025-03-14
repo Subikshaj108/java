e37f26223f807b8dd05a13931b1853c71440cd30
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-14 10:54:57 +0530

    apple 5 times

---
+++ dowhiles.java
@@ -0,0 +1,15 @@
+public class dowhiles
+{
+    public static void main(String[] args)
+    {
+        String str="apple";
+        int i=0;
+        
+        do
+        {
+            System.out.println(str);
+            i++;
+        }
+        while(i<5);
+    }
+}
