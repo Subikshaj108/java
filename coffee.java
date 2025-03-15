d71f1db48c21aa0cce07627399e93d5c63458ee6
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-15 11:13:07 +0530

    type of coffee

---
+++ coffee.java
@@ -0,0 +1,27 @@
+public class coffee
+{
+    public static void main(String[] args)
+    {
+        int choice=4;
+        switch(choice)
+        {
+            case 1:
+            System.out.println("blackcoffee");
+            break;
+            case 2:
+            System.out.println("coldcoffe");
+            break;
+            case 3:
+            System.out.println("dalcano");
+            break;
+            case 4:
+            System.out.println("hotcoffee");
+            break;
+            case 5:
+            System.out.println("thank you for visiting");
+            break;
+            default:
+                System.out.println("choose the coffee");
+        }
+    }
+}
