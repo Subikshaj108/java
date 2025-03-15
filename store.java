b5856ff03afdc07caa377e06acf6768f8354c038
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-15 10:53:35 +0530

    price of product

---
+++ store.java
@@ -0,0 +1,13 @@
+public class store
+{
+    public static void main(String[] args)
+    {
+        
+        int[] price={20,70,50,25,40};
+        for (int i=0;i<=price.length;i++)
+        {
+            System.out.println("price of the product:"+ price[i]);
+           
+        }
+    }
+}
