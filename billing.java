61b7b2e7dae202c319522a4b39baa3274b0327a3
Subikhsaj108 <j.subiksha810@gmail.com>
2025-03-15 10:33:53 +0530

    invoice number

---
+++ billing.java
@@ -0,0 +1,11 @@
+public class billing
+{
+    public static void main (String[] args)
+    {
+        int[] arr={256,289,287,214,298};
+        for(int i=0;i<arr.length;i++)
+        {
+        System.out.println("invoice number of customer :"+arr[0]);
+        }
+    }
+}
